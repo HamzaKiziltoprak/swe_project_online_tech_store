1f8ac64fee99670d43838302ead8e4e3a78360b3
Tuna Katırcı <tunakatirci02@gmail.com>
2025-12-02 17:26:55 +0300

    add models and update dbcontext

--- OnlineTechStore/OnlineTechStore.Server/Data/OnlineTechStoreDbContext.cs
+++ OnlineTechStore/OnlineTechStore.Server/Data/OnlineTechStoreDbContext.cs
@@ -8,5 +8,26 @@ namespace OnlineTechStore.Server.Data
         {
         }
 
+        public DbSet<OnlineTechStore.Server.Models.User> Users { get; set; }
+        public DbSet<OnlineTechStore.Server.Models.Category> Categories { get; set; }
+        public DbSet<OnlineTechStore.Server.Models.Product> Products { get; set; }
+        public DbSet<OnlineTechStore.Server.Models.ProductSpecification> ProductSpecification { get; set; }
+        public DbSet<OnlineTechStore.Server.Models.ProductImage> ProductImages { get; set; }
+        public DbSet<OnlineTechStore.Server.Models.ProductReview> ProductReviews { get; set; }
+        public DbSet<OnlineTechStore.Server.Models.CartItem> CartItems { get; set; }
+        public DbSet<OnlineTechStore.Server.Models.Order> Orders { get; set; }
+        public DbSet<OnlineTechStore.Server.Models.OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>()
+                .HasOne(c => c.ParentCategory)
+                .WithMany(c => c.SubCategories)
+                .HasForeignKey(c => c.ParentCategoryID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    
     }
 }
